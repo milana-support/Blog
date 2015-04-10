a1a111f2729e888f52e55185a6aa663f43c473f0
mimimalizam <milana.stojadinov@gmail.com>
2015-04-10 02:43:17 +0200

    Added Authentication and changed comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, except: [:index, :show]
 
   # GET /posts
   # GET /posts.json
@@ -62,6 +63,7 @@ class PostsController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
@@ -71,4 +73,11 @@ class PostsController < ApplicationController
     def post_params
       params.require(:post).permit(:title, :body)
     end
+
+    def authenticate
+      authenticate_or_request_with_http_basic do |name, password|
+        name == "admin" && pasword == "secret"
+      end
+    end
+
 end
