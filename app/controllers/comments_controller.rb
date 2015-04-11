0915906814434f72cfd840406034cbffe83c0858
mimimalizam <milana.stojadinov@gmail.com>
2015-04-11 23:04:12 +0200

    Ajax2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,9 +30,10 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @post, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        format.json { render action: 'show', status: :created, location: @comment }
+        format.js #create.js.erb
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
@@ -44,7 +45,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.update(comment_params)
         format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
+        format.json { render action: 'show', status: :ok, location: @comment }
       else
         format.html { render :edit }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
